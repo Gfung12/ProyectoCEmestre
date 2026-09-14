487cac2369e32f92d7ab6f42953ac284f8d9079e
Cordero-GG <d.cordero.1@estudiantec.cr>
2026-09-13 22:52:44 -0600

    Revisión correquisitos
    se añadió la lógica para que en caso de que una materia tengo un correquisito, se revisa si este está aprobado o si se puede matricular

--- include/prereq_checker.h
+++ include/prereq_checker.h
@@ -8,6 +8,6 @@
 bool is_course_approved(const char *code, const StudentHistory *history);
 
 //Verifica si el estudiante cumple con todos los prerrequisitos de un curso
-bool can_take_course(const Course *course, const StudentHistory *history);
+bool can_take_course(const Course *course, const StudentHistory *history, const Catalog *catalog);
 
 #endif
--- src/prereq_checker.c
+++ src/prereq_checker.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "../include/struct_definitions.h"
 #include "../include/prereq_checker.h"
-
+//Se debe de tomar en cuenta que si el código del curso no está de la misma forma escrito, se toma como no aprobado
 bool is_course_approved(const char *code, const StudentHistory *history) {
     if (!code || !history) return false;
 
@@ -14,8 +14,8 @@ bool is_course_approved(const char *code, const StudentHistory *history) {
     return false;
 }
 
-bool can_take_course(const Course *course, const StudentHistory *history) {
-    if (!course || !history) return false;
+bool can_take_course(const Course *course, const StudentHistory *history, const Catalog *catalog) { // ACA FALTABA EL CATALOGO
+    if (!course || !history || !catalog) return false;
 
     //Si el estudiante ya aprobó la materia, no necesita volver a llevarla
     if (is_course_approved(course->code, history)) {
@@ -28,6 +28,39 @@ bool can_take_course(const Course *course, const StudentHistory *history) {
             return false;
         }
     }
+
+    //Verifica si el curso necesita corequisitos y si se tiene del mismo
+    for (int i = 0; i < course->coreq_count; i++) {
+        const char *coreq_code = course->corequisites[i];
+
+        if (is_course_approved(coreq_code, history)) {
+            continue;
+        }
+
+        bool can_take_coreq = false; // el curso no se ha aprobado
+
+        // revisar si el correquisito es matriculable
+        for (int j = 0; j < catalog->course_count; j++) {
+            if (strcmp(catalog->courses[j].code, coreq_code) == 0) {
+                can_take_coreq = true;
+
+                
+                for (int k = 0; k < catalog->courses[j].prereq_count; k++) {
+                    if (!is_course_approved(catalog->courses[j].prerequisites[k], history)) {
+                        can_take_coreq = false; // ACA FALTABA ESTO para botar la bandera
+                        break;
+                    }
+                }
+                break; // Ya encontramos el curso, salimos del ciclo j
+            }
+        }//fin de  J
+
+        // ESTO ESTABA ADENTRO DEL CICLO J, TIENE QUE IR AFUERA
+        if (!can_take_coreq) {
+            return false; // Lo pateamos, no puede llevar la materia principal.
+        }
+
+    } // FIN DEL CICLO I (Esta llave faltaba)
 
     return true;
 }
